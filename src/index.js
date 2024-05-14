49e940dc33e43956c09732f09b58f8799a2d17f7
Dan <danporeda@yahoo.com>
2024-05-14 16:14:27 -0400

    imported Provider and store into root index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import { Provider } from 'react-redux';
 import "bulma/css/bulma.css";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import "./store";
+import { store } from "./store";
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  
+);
--- src/store/index.js
+++ src/store/index.js
@@ -17,13 +17,4 @@ const store = configureStore({
   },
 });
 
-const startingState = store.getState();
-console.log(JSON.stringify(startingState));
-
-store.dispatch({
-  type: 'song/addSong',
-  payload: 'New Song'
-});
-
-const finalState = store.getState();
-console.log(JSON.stringify(finalState));
+export { store };
