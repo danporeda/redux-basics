cd65dcd35a4ef036c7b9d2cac68dd553b451cacd
Dan <danporeda@yahoo.com>
2024-05-14 22:05:12 -0400

    completed the movies state and and implemented functionally on the page

--- src/store/index.js
+++ src/store/index.js
@@ -14,11 +14,27 @@ const songsSlice = createSlice({
   },
 });
 
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
 const store = configureStore({
   reducer: {
     songs: songsSlice.reducer,
+    movies: moviesSlice.reducer
   },
 });
 
 export { store };
 export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
